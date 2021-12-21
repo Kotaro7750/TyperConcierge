4f5a1216d43ba09be35abe7b6969e747ea58c2f5
Kotaro7750 <7750koutarou@gmail.com>
2021-12-21 18:00:22 +0900

    initial commit

---
+++ src/main/main.ts
@@ -0,0 +1,38 @@
+import path from 'path';
+import { BrowserWindow, app } from 'electron';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+  const execPath = '../node_modules/.bin/electron';
+
+  require('electron-reload')(__dirname, {
+    electron: path.resolve(__dirname, execPath),
+    forceHardReset: false,
+    hardResetMethod: 'exit',
+  });
+}
+
+// BrowserWindow インスタンスを作成する関数
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.resolve(__dirname, 'preload.js'),
+    },
+  });
+
+  if (isDev) {
+    // 開発モードの場合はデベロッパーツールを開く
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+  }
+
+  // レンダラープロセスをロード
+  mainWindow.loadFile('dist/index.html');
+};
+
+app.whenReady().then(async () => {
+  createWindow();
+});
+
+// すべてのウィンドウが閉じられたらアプリを終了する
+app.once('window-all-closed', () => app.quit());
---
+++ webpack.config.ts
@@ -0,0 +1,75 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const common: Configuration = {
+  mode: isDev ? 'development' : 'production',
+  node: {
+    // メインプロセス内の__dirnameや__filenameをwebpackが変換しないようにする
+    __dirname: false,
+    __filename: false
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /noe_modules/,
+        loader: 'ts-loader'
+      },
+    ]
+  },
+
+  watch: isDev,
+  devtool: isDev ? 'inline-source-map' : undefined
+};
+
+
+// メインプロセス向け設定
+const main: Configuration = {
+  ...common,
+  target: 'electron-main',
+  entry: {
+    main: './src/main/main.ts',
+  },
+};
+
+// プリロードスクリプト向け設定
+const preload: Configuration = {
+  ...common,
+  target: 'electron-preload',
+  entry: {
+    preload: './src/main/preload.ts',
+  },
+};
+
+// レンダラープロセス向け設定
+const renderer: Configuration = {
+  ...common,
+  target: 'web',
+  entry: {
+    renderer: './src/renderer/main.tsx',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: !isDev,
+      inject: 'body',
+      filename: 'index.html',
+      template: './src/renderer/index.html',
+    }),
+  ],
+};
+
+console.log(isDev);
+const config = isDev ? [renderer] : [main, preload, renderer];
+export default config;
