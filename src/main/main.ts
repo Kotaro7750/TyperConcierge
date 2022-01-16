dcebd16a9f7af1d0d6ee9e2f9d5168b0fce5f00a
Kotaro7750 <7750koutarou@gmail.com>
2022-01-16 13:10:43 +0900

    Add: 通常のユーザーにも使いやすいようにメニューとドキュメントを整備した

--- src/main/main.ts
+++ src/main/main.ts
@@ -56,21 +56,115 @@ app.once('window-all-closed', () => {
   }
 });
 
-const menuTemplate: (Electron.MenuItemConstructorOptions | Electron.MenuItem)[] = [
-  {
-    label: '語彙',
-    submenu: [
-      {
-        label: '語彙フォルダを開く',
-        click() {
-          shell.showItemInFolder(libraryManager.libraryDirPath);
-        }
-      }
-    ]
-  }
-];
+const isMac = process.platform === 'darwin';
 
-const menu = Menu.buildFromTemplate(menuTemplate);
+const menu = Menu.buildFromTemplate(
+  [
+    // { role: 'appMenu' }
+    ...(isMac ? [{
+      label: app.name,
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services' },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }] : []) as Electron.MenuItemConstructorOptions[],
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open Vocabulary Folder',
+          click: () => {
+            // showItemInFolderは指定したファイル自体を表示するものなのでディレクトリを指定すると１つ上のディレクトリを開いてしまう
+            // そのためダミーのファイルを指定してやると中に入ってくれる
+            // あってもなくても特にエラーは起こらない
+            shell.showItemInFolder(path.join(libraryManager.libraryDirPath, 'dummy'));
+          }
+        },
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ] as Electron.MenuItemConstructorOptions[]
+    },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac ? [
+          { role: 'pasteAndMatchStyle' },
+          { role: 'delete' },
+          { role: 'selectAll' },
+          { type: 'separator' },
+          {
+            label: 'Speech',
+            submenu: [
+              { role: 'startSpeaking' },
+              { role: 'stopSpeaking' }
+            ]
+          }
+        ] : [
+          { role: 'delete' },
+          { type: 'separator' },
+          { role: 'selectAll' }
+        ]) as Electron.MenuItemConstructorOptions[]
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac ? [
+          { type: 'separator' },
+          { role: 'front' },
+          { type: 'separator' },
+          { role: 'window' }
+        ] : [
+          { role: 'close' }
+        ]) as Electron.MenuItemConstructorOptions[]
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            await shell.openExternal('https://github.com/Kotaro7750/TyperConcierge')
+          }
+        }
+      ]
+    }
+  ]
+);
+
 Menu.setApplicationMenu(menu);
 
 
